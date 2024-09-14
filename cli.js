cb3e83203f44490081c9671a8d7e4719469e39c8
reaper <ahoy@barelyhuman.dev>
2024-09-14 17:35:28 +0530

    fix: issue

--- cli.js
+++ cli.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const { join } = require('path')
 
 let data
 
-const pkg = require(join(__dirname,"./package.json"))
+const pkg = require(join(__dirname, './package.json'))
 
-try{
-    fs.writeFileSync(
-        'package.json',
-        JSON.stringify(
-          Object.assign(
-            {},
-            (data = JSON.parse(fs.readFileSync('package.json', 'utf8'))),
-            {
-              prettier: pkg.name,
-            }
-          )
-          ,null,2
-        ),
-        'utf8'
-      )
-      
-}catch(err){
-    throw new Error(`Failure to modify "prettier" property in package.json`)
-    
+try {
+  fs.writeFileSync(
+    'package.json',
+    JSON.stringify(
+      Object.assign(
+        {},
+        (data = JSON.parse(fs.readFileSync('package.json', 'utf8'))),
+        {
+          prettier: pkg.name,
+        }
+      ),
+      null,
+      2
+    ),
+    'utf8'
+  )
+} catch (err) {
+  throw new Error(`Failure to modify "prettier" property in package.json`)
 }
